53f500928df8dbc7768ebd787daf5fcf61a6b1e5
Breno Menezes <breno.luizmenezes0123@gmail.com>
2023-11-27 17:06:44 -0300

    Try catch consertado

--- src/ecommerce/Menu.java
+++ src/ecommerce/Menu.java
@@ -9,13 +9,15 @@ public class Menu {
 		
 		Scanner ler = new Scanner(System.in);
 		
-		int opcao = 0;
+		int opcao;
 		
 		int logon = 0, perms = 0, tempInt;
 		boolean continua = true;
 		String usuario, senha, temp;
 		
 		while(continua) {
+			
+			ler.nextLine();
 			if (logon == 0) {
 				System.out.println("====================================================");
 				System.out.println("                                                    ");
@@ -30,34 +32,34 @@ public class Menu {
 				System.out.print("Entre com a opção desejada: ");
 				try {
 					opcao = ler.nextInt();
-				} catch (InputMismatchException e) {
 					
 					switch(opcao) {
-					case 1 -> {
-						ler.nextLine();
-						System.out.println("                                                    ");
-						System.out.print("Insira o usuário: ");
-						usuario = ler.nextLine();
-						System.out.println("                                                    ");
-						//ler.nextLine();
-						System.out.print("Insira a senha: ");
-						senha = ler.nextLine();
-						
-						if(usuario.equals("1") && senha.equals("1")) {
-							//FUNÇÃO PARA CHECAR O NOME E SE É ADMINISTRADOR
-							perms = 1;
-							logon = 1;
-						} else if (usuario.equals("2") && senha.equals("2")) {
-							//FUNÇÃO PARA CHECAR O NOME E SE É COMPRADOR
-							perms = 2;
-							logon = 1;
+						case 1 -> {
+							ler.nextLine();
+							System.out.println("                                                    ");
+							System.out.print("Insira o usuário: ");
+							usuario = ler.nextLine();
+							System.out.println("                                                    ");
+							//ler.nextLine();
+							System.out.print("Insira a senha: ");
+							senha = ler.nextLine();
+							
+							if(usuario.equals("1") && senha.equals("1")) {
+								//FUNÇÃO PARA CHECAR O NOME E SE É ADMINISTRADOR
+								perms = 1;
+								logon = 1;
+							} else if (usuario.equals("2") && senha.equals("2")) {
+								//FUNÇÃO PARA CHECAR O NOME E SE É COMPRADOR
+								perms = 2;
+								logon = 1;
+							}
+						}
+						case 2 -> {
+							continua = false;
 						}
 					}
-					case 2 -> {
-						continua = false;
-					}
-				}
-					
+				} catch (InputMismatchException e) {
+					System.out.println("\nInsira um número como opção!");
 				}
 			} else {
 				switch(perms) {
@@ -84,56 +86,57 @@ public class Menu {
 						System.out.print("Entre com a opção desejada: ");
 						try {
 							opcao = ler.nextInt();
-						} catch (InputMismatchException e) {
+							
 							switch(opcao) {
-							case 1 -> {
-								System.out.println("CADASTRA");
+								case 1 -> {
+									System.out.println("CADASTRA");
+								}
+								case 2 -> {
+									System.out.println("DELETA");
+								}
+								case 3 -> {
+									System.out.println("LISTA");
+								}
+								case 4 -> {
+									System.out.println("BUSCA");
+								}
+								case 5 -> {
+									System.out.println("ATUALIZA");
+								}
+								case 6 -> {
+									System.out.println("ESTOCA");
+								}
+								case 7 -> {
+									System.out.println("REMOVE");
+								}
+								case 8 -> {
+									ler.nextLine();
+									System.out.println("                                                    ");
+									System.out.print("Insira o nome do usuário: ");
+									temp = ler.nextLine();
+									System.out.println("                                                    ");
+									System.out.print("Insira a senha do usuário: ");
+									temp = ler.nextLine();
+									System.out.println("                                                    ");
+									System.out.print("Insira o nível de permissão: ");
+									tempInt = ler.nextInt();
+										//CADASTRAR OS DADOS NO OBJETO
+								}
+								case 9 -> {
+									System.out.println("                                                    ");
+									System.out.print("Insira o nome do usuário a ser removido: ");
+									temp = ler.nextLine();
+								}
+								case 10 -> {
+									logon = 0;
+									perms = 0;
+									usuario = "0";
+									senha = "0";
+								}
 							}
-							case 2 -> {
-								System.out.println("DELETA");
-							}
-							case 3 -> {
-								System.out.println("LISTA");
-							}
-							case 4 -> {
-								System.out.println("BUSCA");
-							}
-							case 5 -> {
-								System.out.println("ATUALIZA");
-							}
-							case 6 -> {
-								System.out.println("ESTOCA");
-							}
-							case 7 -> {
-								System.out.println("REMOVE");
-							}
-							case 8 -> {
-								ler.nextLine();
-								System.out.println("                                                    ");
-								System.out.print("Insira o nome do usuário: ");
-								temp = ler.nextLine();
-								System.out.println("                                                    ");
-								System.out.print("Insira a senha do usuário: ");
-								temp = ler.nextLine();
-								System.out.println("                                                    ");
-								System.out.print("Insira o nível de permissão: ");
-								tempInt = ler.nextInt();
-									//CADASTRAR OS DADOS NO OBJETO
-							}
-							case 9 -> {
-								System.out.println("                                                    ");
-								System.out.print("Insira o nome do usuário a ser removido: ");
-								temp = ler.nextLine();
-							}
-							case 10 -> {
-								logon = 0;
-								perms = 0;
-								usuario = "0";
-								senha = "0";
-							}
+						} catch (InputMismatchException e) {
+							System.out.println("Insira um número como opção!");
 						}
-						}
-						
 					}
 					case 2 -> {
 						System.out.println("====================================================");
@@ -152,27 +155,28 @@ public class Menu {
 						System.out.print("Entre com a opção desejada: ");
 						try {
 							opcao = ler.nextInt();
-						} catch (InputMismatchException e) {
-							switch(opcao) {
-							case 1 -> {
-								System.out.println("LISTA");
-							}
-							case 2 -> {
-								System.out.println("BUSCA");
-							}
-							case 3 -> {
-								System.out.println("COMPRA");
-							}
-							case 4 -> {
-								logon = 0;
-								perms = 0;
-								usuario = "0";
-								senha = "0";
-							}
 							
+							switch(opcao) {
+								case 1 -> {
+									System.out.println("LISTA");
+								}
+								case 2 -> {
+									System.out.println("BUSCA");
+								}
+								case 3 -> {
+									System.out.println("COMPRA");
+								}
+								case 4 -> {
+									logon = 0;
+									perms = 0;
+									usuario = "0";
+									senha = "0";
+								}
+								
+							}
+						} catch (InputMismatchException e) {
+							System.out.println("Insira um número como opção!");
 						}
-						}
-						
 					}
 				}
 			}
